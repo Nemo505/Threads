cac74352974163aaf742207f2234a12af9e8061e
nemo505 <dahliahalesia8@gmail.com>
2023-12-13 14:19:52 +0630

    migration

---
+++ config/Migrations/20231213071419_User.php
@@ -0,0 +1,42 @@
+<?php
+declare(strict_types=1);
+
+use Migrations\AbstractMigration;
+
+class User extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
+     * @return void
+     */
+    public function change(): void
+    {
+        $userTable = $this->table('users');
+        
+        $userTable->addColumn('name', 'string', [
+            'default' => null,
+            'limit' => 255,
+            'null' => false,
+        ]);
+
+        $userTable->addColumn('password', 'string', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $userTable->addColumn('created', 'datetime', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $userTable->addColumn('modified', 'datetime', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $userTable->create();
+
+        
+        
+    }
+}
